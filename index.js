7b549377abfd761a2c0de09ede88d25e3c1ba80d
JannisKohle <Jannis.Kohle@icloud.com>
2020-07-06 21:10:58 +0200

    started index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const usersRoutes = require("./routes/users");
+const diariesRoutes = require("./routes/diaries");
+
+const app = express();
+
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/diaries", diariesRoutes);
