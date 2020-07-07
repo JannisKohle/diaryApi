8622a06b5a5b64e85f645b41859b80af0fe11920
JannisKohle <Jannis.Kohle@icloud.com>
2020-07-07 14:19:07 +0200

    fixed another little bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const diariesRoutes = require("./routes/diaries");
 const app = express();
 
 app.use(express.json());
-app.use("/users", userRoutes);
+app.use("/users", usersRoutes);
 app.use("/diaries", diariesRoutes);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
