24576a7772b3a1c668cfb1d3881e6e72f338fa80
JannisKohle <Jannis.Kohle@icloud.com>
2020-07-07 14:03:03 +0200

    added app.listen() to index.js

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/diaries", diariesRoutes);
+
+app.listen(3000, () => console.log("Listening on port 3000"));
